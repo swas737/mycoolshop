1d5575abb612f2d5c0ee978c07b04db0e66b868f
swastika <sswas737@gmail.com>
2023-04-03 13:43:38 +0530

    persist added

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit"
-import shopReducer from "./Reducer/shopSlice"
-
-export default configureStore({
-  reducer: {
-    shop: shopReducer,
-  },
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import shopReducer from '../utils/Reducer/shopSlice'
+const rootReducer = combineReducers({
+  shop: shopReducer,
 })
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+})
+
+export default store
